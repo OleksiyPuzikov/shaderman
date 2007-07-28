aa66d62dbcca813500fa3f863215ddd82e0e316f
puzikov <puzikov@da488fbe-a924-0410-aab6-97118626d01c>
2007-07-28 19:12:12 +0000

    Image was broken on About window when you've been launching Shaderman not from it's directory.

--- core/about.py
+++ core/about.py
@@ -3,13 +3,14 @@
 import sys
 import wx
 import wx.html
+from core.utils import good_path
 
 class AboutBox(wx.Dialog):
 	text = """
 <html>
 <body bgcolor="#EFEFEF" border="0" style="padding: 0; margin: 0;">
 <center>
-<img src="core/logo.gif" />
+<img src="%s" />
 <br>
 <font size="-2">
 &copy; Alexei Puzikov, http://www.dream.com.ua<br>
@@ -19,7 +20,7 @@ wxPython %s (%s), running on Python %s</font></center></body></html>"""
 	def __init__(self, parent):
 		wx.Dialog.__init__(self, parent, -1, 'About ShaderMan.Next',)
 		html = wx.html.HtmlWindow(self, -1, size=(420, -1))
-		html.SetPage(self.text % (wx.VERSION_STRING, wx.PlatformInfo[1], sys.version.split()[0]))
+		html.SetPage(self.text % (good_path("core/logo.gif"), wx.VERSION_STRING, wx.PlatformInfo[1], sys.version.split()[0]))
 		
 		ir = html.GetInternalRepresentation()
 		html.SetSize( (ir.GetWidth()+20, ir.GetHeight()+20) )
