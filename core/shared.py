7275a3fbb4279d96a8dfbc3a2cc80271f99b616e
puzikov <puzikov@da488fbe-a924-0410-aab6-97118626d01c>
2007-08-20 00:55:03 +0000

    Deprecated GetNextNodeID() and GetNextConnectionID()

--- core/shared.py
+++ core/shared.py
@@ -9,10 +9,28 @@ arrows = []
 nodes = []
 connections = []
 
+groups = []
+
 settings = {}
 
-def GetNextNodeID():
+import warnings
+
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emmitted
+    when the function is used."""
+    def newFunc(*args, **kwargs):
+        warnings.warn("Call to deprecated function %s." % func.__name__, category=DeprecationWarning, stacklevel=2)
+        return func(*args, **kwargs)
+    newFunc.__name__ = func.__name__
+    newFunc.__doc__ = func.__doc__
+    newFunc.__dict__.update(func.__dict__)
+    return newFunc
+
+@deprecated
+def GetNextNodeID(): # those are deprecated
 	pass
 
+@deprecated
 def GetNextConnectionID():
 	pass
